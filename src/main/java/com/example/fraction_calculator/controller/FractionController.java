cf8f4838c8f17b56ad681309bfaab3aa638d75ec
luricisme <trinhnguyenluong182306@gmail.com>
2025-11-10 17:18:16 +0700

    Test CI/CD dev #1

--- src/main/java/com/example/fraction_calculator/controller/FractionController.java
+++ src/main/java/com/example/fraction_calculator/controller/FractionController.java
@@ -90,4 +90,12 @@ public class FractionController {
         public String getError() { return error; }
         public void setError(String error) { this.error = error; }
     }
+
+    // API test dev
+    @GetMapping("/hello-dev")
+    @Operation(summary = "Test hello-dev endpoint")
+    @ApiResponse(responseCode = "200", description = "Trả về thông điệp hello-dev")
+    public ResponseEntity<String> helloDev() {
+        return ResponseEntity.ok("Hello Dev! This is the dev environment.");
+    }
 }
